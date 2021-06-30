3fffcdacf79d80c871d6ff456a3e815a3a9c591f
Ben Dalling <b.dalling@locp.co.uk>
2021-06-30 19:59:39 +0100

    Give them RADAR-GUIDED SKEE-BALL LANES and VELVEETA BURRITOS!!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -e
 echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
 echo "GITHUB_REF: ${GITHUB_REF}"
-gitchangelog
+gitchangelog > CHANGELOG.md
+changed_files=$( git status -s )
+
+if [ $changed_files -ne '0' ]; then
+  echo 'The CHANGELOG.md is out of date.'
+  exit 1
+fi
+
 exit 0
